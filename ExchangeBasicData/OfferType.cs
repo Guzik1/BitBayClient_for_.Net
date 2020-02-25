7401fb3eb292f11b4a6c3b529b7ecfa26fa17e8f
Sebastian Guzik <guzikpack.kontakt@gmail.com>
2020-02-25 20:19:05 +0100

    Add exchange basic data.

---
+++ ExchangeBasicData/OfferType.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExchangeBasicData
+{
+    public enum OfferType
+    {
+        Limit,
+        Market,
+
+        Stop_limit,
+        Stop_market
+    }
+
+    public static class OfferTypeExtension
+    {
+        public static OfferType Parse(string name)
+        {
+            string toParse = name.Replace('-', '_');
+
+            return (OfferType)Enum.Parse(typeof(OfferType), toParse, true);
+        }
+
+        public static string ToLowerString(this OfferType offerType)
+        {
+            string output = offerType.ToString().ToLower();
+
+            return output.Replace('_', '-');
+        }
+    }
+}
---
+++ ExchangeBasicData/TransactionType.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExchangeBasicData
+{
+    public enum TransactionType
+    {
+        Buy,
+        Sell
+    }
+
+    public static class TransactionTypeExtensions
+    {
+        public static TransactionType Parse(string name)
+        {
+            return (TransactionType)Enum.Parse(typeof(TransactionType), name);
+        }
+
+        public static int ToInt(this TransactionType type)
+        {
+            return (int)type;
+        }
+
+        public static string ToLowerString(this TransactionType type)
+        {
+            return type.ToString().ToLower();
+        }
+    }
+}
