192b18df14d33a1f431768b83f613d807140e9eb
Sebastian Guzik <guzikpack.kontakt@gmail.com>
2020-03-04 13:56:37 +0100

    Add response checker.

---
+++ BitBayClient/Internal/ResponseChecker.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using RestApiClient;
+
+namespace BitBayClient.Internal
+{
+    public class ResponseChecker : IResponseChecker
+    {
+        public bool CheckResponseIsOk(string response)
+        {
+            if (response.Contains("\"status\":\"Fail\""))
+                return false;
+
+            return true;
+        }
+    }
+}
