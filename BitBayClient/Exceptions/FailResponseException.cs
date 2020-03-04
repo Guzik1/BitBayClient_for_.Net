16d163b247ac4677136de4583b3786fefff3f739
Sebastian Guzik <guzikpack.kontakt@gmail.com>
2020-03-04 19:19:59 +0100

    Add exception (fail/error response).

---
+++ BitBayClient/Exceptions/FailResponseException.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BitBayClient.Exceptions
+{
+    [Serializable]
+    public class FailResponseException : Exception
+    {
+        public List<ErrorMessage> Errors = new List<ErrorMessage>();
+
+        public FailResponseException() : base() { }
+
+        public FailResponseException(List<ErrorMessage> errors) : base()
+        {
+            Errors = errors;
+        }
+
+        public FailResponseException(string message) : base(message) { }
+
+        public FailResponseException(string message, List<ErrorMessage> errors) : base(message)
+        {
+            Errors = errors;
+        }
+
+        public FailResponseException(string message, Exception inner) : base(message, inner) { }
+
+        public FailResponseException(string message, Exception inner, List<ErrorMessage> errors) : base(message, inner)
+        {
+            Errors = errors;
+        }
+    }
+}
