2182d103f33b27eca4f82d8947aee45c8ffcf005
Sebastian Guzik <guzikpack.kontakt@gmail.com>
2020-03-09 21:58:42 +0100

    Update config

--- BitBayClient/Config.cs
+++ BitBayClient/Config.cs
@@ -6,6 +6,10 @@ namespace BitBayClient
     {
         internal string ApiTradingUrl => ApiUrl + "trading/";
 
+        internal string ApiTradingStopUrl => ApiTradingUrl + "stop/";
+
+        internal string ApiBalancesUrl => ApiUrl + "balances/BITBAY/balance/";
+
         internal string ApiUrl { get; } = "https://api.bitbay.net/rest/";
 
         internal string PublicKey { get; set; } = "";
