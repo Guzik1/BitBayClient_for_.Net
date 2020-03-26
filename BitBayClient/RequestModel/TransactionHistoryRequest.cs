3295b53db62ef314663fccd0a2136cc91873e79c
Sebastian Guzik <guzikpack.kontakt@gmail.com>
2020-03-26 21:44:18 +0100

    Add docs to transaction history data model.

--- BitBayClient/RequestModel/TransactionHistoryRequest.cs
+++ BitBayClient/RequestModel/TransactionHistoryRequest.cs
@@ -7,25 +7,49 @@ using BitBayClient.Converters;
 
 namespace BitBayClient.RequestModel
 {
+    /// <summary>
+    /// Data model for get transaction history, is parameter in <see cref="BitBayClient.GetTransactionHistory(TransactionHistoryRequest)"/>, use to filter resault.
+    /// </summary>
     public class TransactionHistoryRequest
     {
-        public List<string> Markets { get; set; }
+        /// <summary>
+        /// List of markets code. Use for filter.
+        /// </summary>
+        public List<string> Markets { get; set; } = new List<string>();
 
+        /// <summary>
+        /// Next page cursor, use for get next pages of resault. Default cursor is start.
+        /// </summary>
         [JsonProperty("nextPageCursor")]
         public string NextPageCursor { get; set; } = "start";
 
+        /// <summary>
+        /// Rate from filter.
+        /// </summary>
         [JsonProperty("rateFrom")]
         public decimal RateFrom { get; set; }
 
+        /// <summary>
+        /// Rate to filter.
+        /// </summary>
         [JsonProperty("rateTo")]
         public decimal RateTo { get; set; }
 
+        /// <summary>
+        /// From time filter (unix).
+        /// </summary>
         [JsonProperty("fromTime", NullValueHandling = NullValueHandling.Ignore)]
         public long FromTime { get; set; }
 
+        /// <summary>
+        /// To time filter (unix).
+        /// </summary>
         [JsonProperty("toTime", NullValueHandling = NullValueHandling.Ignore)]
         public long ToTime { get; set; }
 
+        /// <summary>
+        /// Filter, user action: buy or sell.
+        /// </summary>
         [JsonProperty("userAction", NullValueHandling = NullValueHandling.Ignore)]
         public string UserAction { get; set; }
     }
--- BitBayClient/ResponseModel/TransactionHistory.cs
+++ BitBayClient/ResponseModel/TransactionHistory.cs
@@ -1,48 +1,91 @@
-﻿using CurrencyPair;
-using ExchangeBasicData;
-using Newtonsoft.Json;
+﻿using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace BitBayClient.ResponseModel
 {
+    /// <summary>
+    /// Data model on get transaction history request, return from <see cref="BitBayClient.GetTransactionHistory(RequestModel.TransactionHistoryRequest)"/>
+    /// </summary>
     public class TransactionHistory
     {
+        /// <summary>
+        /// Number of rows in resault.
+        /// </summary>
         [JsonProperty("totalRows")]
         public int TotalRows { get; set; }
 
+        /// <summary>
+        /// List of transacton history items.
+        /// </summary>
         [JsonProperty("items")]
         public List<TransactionHistoryItem> Transactions { get; set; }
 
+        /// <summary>
+        /// Cursor to next pager of resault, use in request to get next pages.
+        /// </summary>
         [JsonProperty("nextPageCursor")]
         public string NextPageCursor { get; set; }
     }
 
+    /// <summary>
+    /// Transaction history item data model, represent one transaction.
+    /// </summary>
     public class TransactionHistoryItem
     {
+        /// <summary>
+        /// Transaction UUID identificator.
+        /// </summary>
         public string Id { get; set; }
 
+        /// <summary>
+        /// Market code.
+        /// </summary>
         public string Market { get; set; }
 
+        /// <summary>
+        /// The transaction execution time (unix).
+        /// </summary>
         public long Time { get; set; }
 
+        /// <summary>
+        /// Amount of currency in the transaction.
+        /// </summary>
         public decimal Amount { get; set; }
 
+        /// <summary>
+        /// The transation rate.
+        /// </summary>
         public decimal Rate { get; set; }
 
+        /// <summary>
+        /// Who initiated the transaction: buy or sell.
+        /// </summary>
         [JsonProperty("initializedBy")]
         public string InitialBy { get; set; }
 
-        [JsonProperty("wasTaker")]
-        public bool WasTaker { get; set; }
-
+        /// <summary>
+        /// The role user play in executed the transaction: buy or sell.
+        /// </summary>
         [JsonProperty("userAction")]
         public string UserActionType { get; set; }
 
+        /// <summary>
+        /// Is the user completing a existing offer of another user? Flag (true - taker, false - maker).
+        /// </summary>
+        [JsonProperty("wasTaker")]
+        public bool WasTaker { get; set; }
+
+        /// <summary>
+        /// Offer UUID identificator.
+        /// </summary>
         [JsonProperty("offerId")]
         public string OfferId { get; set; }
-
+        
+        /// <summary>
+        /// Value of commission charged.
+        /// </summary>
         [JsonProperty("commissionValue")]
         public decimal CommissionValue { get; set; }
     }
