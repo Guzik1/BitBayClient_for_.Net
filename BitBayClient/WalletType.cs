414f5160eaeedc0892180d7e6a6c8c8993c54238
Sebastian Guzik <guzikpack.kontakt@gmail.com>
2020-03-27 20:37:43 +0100

    Add wallet type enum.

---
+++ BitBayClient/WalletType.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BitBayClient
+{
+    public enum WalletType
+    {
+        CRYPTO,
+        FIAT
+    }
+
+    public static class WalletTypeExtension
+    {
+        public static WalletType Parse(string name)
+        {
+            return (WalletType)Enum.Parse(typeof(WalletType), name);
+        }
+    }
+}
