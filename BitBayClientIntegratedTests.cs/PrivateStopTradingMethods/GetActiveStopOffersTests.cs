063cbce70b0af355b0355763c3786049172736e4
Sebastian Guzik <guzikpack.kontakt@gmail.com>
2020-04-02 12:45:30 +0200

    Add private stop offers tests.

---
+++ BitBayClientIntegratedTests.cs/PrivateStopTradingMethods/GetActiveStopOffersTests.cs
@@ -0,0 +1,35 @@
+﻿using BitBayClient.Exceptions;
+using BitBayClient.ResponseModel;
+using BitBayClientTests;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BitBayClientIntegratedTests.PrivateStopTradingMethods
+{
+    public class GetActiveStopOffersTests
+    {
+        BitBayClient.BitBayClient bbClient = Tools.SetUp();
+        List<ActiveStopOffer> offers;
+
+        [Test]
+        public void GetActiveOffersTest()
+        {
+            try
+            {
+                offers = bbClient.PrivateStopTrading.GetActiveStopOffer("BTC-PLN");
+            }
+            catch (FailResponseException e)
+            {
+                Tools.HandlingErrors(e);
+            }
+
+            Assert.IsNotNull(offers);
+            if (offers.Count > 0)
+            {
+                Assert.AreEqual("BTC-PLN", offers[0].Market);
+            }
+        }
+    }
+}
---
+++ BitBayClientIntegratedTests.cs/PrivateStopTradingMethods/StopOfferHandlingTests.cs
@@ -0,0 +1,92 @@
+﻿using BitBayClient.Exceptions;
+using BitBayClient.RequestModel;
+using BitBayClient.ResponseModel;
+using BitBayClientTests;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BitBayClientIntegratedTests.PrivateStopTradingMethods
+{
+    public class StopOfferHandlingTests
+    {
+        BitBayClient.BitBayClient bbClient = Tools.SetUp();
+        string offerId;
+        decimal amount = 1;
+        decimal rate = 5;
+        decimal stopRate = 6;
+
+        bool runTest = false;
+
+        [Test]
+        public void StopOfferManipTest()
+        {
+            if (runTest && Config.IsAutorized())
+            {
+                AddNewStopOffer();
+                if (CheckStopOfferIdAdded())
+                {
+                    Assert.IsTrue(RemoveStopOffer());
+                    Assert.IsFalse(CheckStopOfferIdAdded());
+                }
+                else
+                    Assert.Fail();
+            }
+            else
+                Assert.IsTrue(true);
+        }
+
+        void AddNewStopOffer()
+        {
+            NewStopOfferRequest body = new NewStopOfferRequest();
+            body.SetStopLimitOffer("BUY", amount, stopRate, rate);
+
+            try
+            {
+                offerId = bbClient.PrivateStopTrading.AddNewStopOffer("BTC-PLN", body);
+            }
+            catch (FailResponseException e)
+            {
+                Tools.HandlingErrors(e);
+                return;
+            }
+        }
+
+        bool CheckStopOfferIdAdded()
+        {
+            List<ActiveStopOffer> offers;
+
+            try
+            {
+                offers = bbClient.PrivateStopTrading.GetActiveStopOffer("BTC-PLN");
+            }
+            catch (FailResponseException e)
+            {
+                Tools.HandlingErrors(e);
+                return false;
+            }
+
+            if(offers.Count > 0)
+            {
+                int index = offers.FindIndex(n => n.Id == offerId);
+                return index != -1;
+            }
+
+            return false;
+        }
+
+        bool RemoveStopOffer()
+        {
+            try
+            {
+                return bbClient.PrivateStopTrading.DeleteStopOffer("BTC-PLN", offerId);
+            }
+            catch (FailResponseException e)
+            {
+                Tools.HandlingErrors(e);
+                return false;
+            }
+        }
+    }
+}
